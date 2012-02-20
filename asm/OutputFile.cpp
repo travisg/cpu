e01fe0e3a17b1d982a2734e3a3b73bb466ac5550
Travis Geiselbrecht <geist@foobox.com>
2012-02-20 00:45:04 -0800

    [asm] add a header to make linux happy

--- asm/OutputFile.cpp
+++ asm/OutputFile.cpp
@@ -20,6 +20,7 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#include <cstring>
 #include <string>
 #include <stdio.h>
 #include <assert.h>
