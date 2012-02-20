f3f3ec73ec9ecac13808243a73b4068105da55fd
Travis Geiselbrecht <geist@foobox.com>
2012-02-20 15:50:09 -0800

    [asm] run the file through the cpp preprocesor first
    - add support for # prefixed lines, which the preprocessor likes to drop

--- asm/main.cpp
+++ asm/main.cpp
@@ -23,6 +23,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <getopt.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "Sym.h"
 #include "Symtab.h"
@@ -34,8 +36,9 @@ Codegen *gCodegen;
 
 static std::string output_filename;
 
-extern int open_input(const char *file);
+extern int open_input(int fd, const char *file);
 extern int parse_source();
+static int preprocess(const std::string &file);
 
 static void usage(int argc, char **argv)
 {
@@ -79,7 +82,14 @@ int main(int argc, char **argv)
 	argc -= optind;
 	argv += optind;
 
-	if (open_input(argv[0]) < 0) {
+	// start preprocessor
+	int preprocess_out = preprocess(argv[0]);
+	if (preprocess_out < 0) {
+		fprintf(stderr, "error starting preprocessor\n");
+		return 1;
+	}
+
+	if (open_input(preprocess_out, argv[0]) < 0) {
 		fprintf(stderr, "error opening input file\n");
 		return 1;
 	}
@@ -108,9 +118,67 @@ int main(int argc, char **argv)
 	gCodegen->FixupPass();
 
 err:
+	close(preprocess_out);
 	delete gCodegen;
 	delete gSymtab;
 	delete f;
 
 	return err;
 }
+
+static int preprocess(const std::string &file)
+{
+
+	int fd = open(file.c_str(), O_RDONLY);
+//	printf("fd %d\n", fd);
+	if (fd < 0)
+		return -1;
+
+	int pipes[2];
+	if (pipe(pipes) < 0) {
+		close(fd);
+		return -1;
+	}
+
+//	printf("pipes %d %d\n", pipes[0], pipes[1]);
+
+	pid_t pid = fork();
+	if (pid == 0) {
+		// child
+//		printf("child\n");
+
+		close(STDOUT_FILENO);
+		close(STDIN_FILENO);
+
+		dup2(fd, STDIN_FILENO);
+		dup2(pipes[1], STDOUT_FILENO);
+
+		close(fd);
+		close(pipes[0]);
+		close(pipes[1]);
+
+		execlp("cpp", 0);
+
+		fprintf(stderr, "error execing preprocessor\n");
+		exit(1);
+	} else {
+		// parent
+//		printf("parent\n");
+
+		fflush(stdout);
+
+		close(pipes[1]);
+		close(fd);
+
+#if 0
+		char buf[64];
+		int e;
+		while ((e = read(pipes[0], buf, sizeof(buf))) > 0) {
+			write(STDOUT_FILENO, buf, e);
+		}
+#endif
+	}
+
+	return pipes[0];
+}
+
---
+++ test/preprocessor.asm
@@ -0,0 +1,8 @@
+#define CONST 99
+
+label:
+	li 	r0, #CONST
+
+#if 0
+	shouldfail
+#endif
