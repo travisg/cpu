332b3f47f3e4f11be43fd097faab5404872e35aa
Travis Geiselbrecht <geist@foobox.com>
2012-02-20 03:48:11 -0800

    [test] make sure store can handle a offset

--- test/test2.asm
+++ test/test2.asm
@@ -1,6 +1,6 @@
 ; location of the debug 
 	li	r0, #0
-	li	r9, #256
+	li	r9, #252
 bigloop:
 	add	r0, r0, #1
 
@@ -9,6 +9,9 @@ loop:
 	sub r1, r1, #1
 	bnz r1, loop
 
-	str	r0, [r9, #0]
+	str	r0, [r9, #4]
 
 	b bigloop
+
+data:
+	.word #99
